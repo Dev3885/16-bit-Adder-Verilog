e18f224b29f8768297c765fea20b51668a9aeeb1
Debmalya Das <debmalyadas3885@gmail.com>
2026-07-26 11:50:19 +0530

    Implement overflow, zero, and parity checks in add16
    Added checks for overflow, zero flag, parity flag, and eparity in the add16 module.

--- rtl/add16.v
+++ rtl/add16.v
@@ -1,4 +1,3 @@
-// overflow, zero flag, parity flagg check is missing. add them
 module add16 (input wire [15:0] A, B, input wire CIN, output wire [15:0] SUM, output wire COUT, sign, overflow, parity, eparity, zeroflag);
 wire COUT1, COUT2, COUT3;
 assign sign = SUM[15];
@@ -13,3 +12,26 @@ add4 A3 (.s(SUM[11:8]),.c_out(COUT3),.x(A[11:8]),.y(B[11:8]),.c_in(COUT2));
 add4 A4 (.s(SUM[15:12]),.c_out(COUT),.x(A[15:12]),.y(B[15:12]),.c_in(COUT3));
 
 endmodule
+
+
+////adder without the intermidiate adders
+
+//module add16 (input wire [15:0] A, B, input wire CIN, output wire [15:0] SUM, output wire COUT, sign, overflow, parity, eparity, zeroflag);
+//wire [16:0] c;
+//assign c[0]= CIN;
+//assign COUT = c[16];
+//assign sign = SUM[15];
+//assign overflow = (A[15] & B[15] & ~SUM[15]) | (~A[15] & ~B[15] & SUM[15]);
+//assign zeroflag = (SUM == 16'h000);
+//assign parity = ^SUM;
+//assign eparity = ~(^SUM);
+
+//genvar i;
+//generate
+//    for (i=0; i<16; i=i+1)begin: full_chain
+//    fulladder A1 (.sum(SUM[i]),.cout(i),.a(A[i]),.b(B[i]),.cin(i));
+//    end
+//endgenerate
+//endmodule
+
+
